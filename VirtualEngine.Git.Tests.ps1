db39c8814abfbf4bb834119529922dc1be277ada
Iain Brighton <iainbrighton@hotmail.com>
2015-02-26 10:01:07 +0000

    Fixed mocking in Get-GitTag and Get-GitRevision tests Fixed test calls to ConvertToAssemblyVersion

--- VirtualEngine.Git.Tests.ps1
+++ VirtualEngine.Git.Tests.ps1
@@ -3,65 +3,64 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 . "$here\$sut"
 
 Describe "Get-GitTag" {
-    Mock Invoke-Expression { Write-Output '1.0.0' } -ParameterFilter { Command -eq 'git.exe describe --abbrev=0 --tags' }
+    Mock Invoke-Expression { Write-Output '1.0.0' } -ParameterFilter { $Command -eq 'git.exe describe --abbrev=0 --tags' }
 
     It "calls 'git.exe describe --abbrev=0 --tags'." {
         Get-GitTag | Should Be '1.0.0';
-        Assert-MockCalled -CommandName Invoke-Expression -ParameterFilter { Command -eq 'git.exe descrive --abbrev=0 --tags' };
+        Assert-MockCalled -CommandName Invoke-Expression -ParameterFilter { $Command -eq 'git.exe describe --abbrev=0 --tags' };
     }
 }
 
 Describe "Get-GitRevision" {
-    Mock Invoke-Expression { Write-Output 48 } -ParameterFilter { Command -eq 'git.exe rev-list HEAD --count' }
+    Mock Invoke-Expression { Write-Output 48 } -ParameterFilter { $Command -eq 'git.exe rev-list HEAD --count' }
 
     It "calls 'git.exe rev-list HEAD --count'." {
         Get-GitRevision | Should Be '48';
-        Assert-MockCalled -CommandName Invoke-Expression -ParameterFilter { Command -eq 'git.exe rev-list HEAD --count' };
+        Assert-MockCalled -CommandName Invoke-Expression -ParameterFilter { $Command -eq 'git.exe rev-list HEAD --count' };
     }
 }
 
 Describe "ConvertToAssemblyVersionArray" {
 
     It "converts 1.2 to 1,2,0,0" {
-        [String]::Join(',', (Convert-ToVersionArray -InputObject '1.2')) | Should Be ([String]::Join(',', @(1,2,0,0)));
+        [String]::Join(',', (ConvertToAssemblyVersionArray -InputObject '1.2')) | Should Be ([String]::Join(',', @(1,2,0,0)));
     }
 
     It "converts 1.2.3 to 1,2,3,0" {
-        [String]::Join(',', (Convert-ToVersionArray -InputObject '1.2.3')) | Should Be ([String]::Join(',', @(1,2,3,0)));
+        [String]::Join(',', (ConvertToAssemblyVersionArray -InputObject '1.2.3')) | Should Be ([String]::Join(',', @(1,2,3,0)));
     }
 
     It "converts 1.2.3.4 to 1,2,3,4" {
-        [String]::Join(',', (Convert-ToVersionArray -InputObject '1.2.3.4')) | Should Be ([String]::Join(',', @(1,2,3,4)));
+        [String]::Join(',', (ConvertToAssemblyVersionArray -InputObject '1.2.3.4')) | Should Be ([String]::Join(',', @(1,2,3,4)));
     }
     
     It "converts prefixed v1.2.3.4 to 1,2,3,4" {
-        [String]::Join(',', (Convert-ToVersionArray -InputObject 'v1.2.3.4')) | Should Be ([String]::Join(',', @(1,2,3,4)));
+        [String]::Join(',', (ConvertToAssemblyVersionArray -InputObject 'v1.2.3.4')) | Should Be ([String]::Join(',', @(1,2,3,4)));
     }
 
     It "converts prefixed V1.2.3.4 to 1,2,3,4" {
-        [String]::Join(',', (Convert-ToVersionArray -InputObject 'V1.2.3.4')) | Should Be ([String]::Join(',', @(1,2,3,4)));
+        [String]::Join(',', (ConvertToAssemblyVersionArray -InputObject 'V1.2.3.4')) | Should Be ([String]::Join(',', @(1,2,3,4)));
     }
 
     It "truncates 1.2.3.4.5 to 1,2,3,4" {
-        [String]::Join(',', (Convert-ToVersionArray -InputObject '1.2.3.4.5' -WarningAction SilentlyContinue)) | Should Be ([String]::Join(',', @(1,2,3,4)));
+        [String]::Join(',', (ConvertToAssemblyVersionArray -InputObject '1.2.3.4.5' -WarningAction SilentlyContinue)) | Should Be ([String]::Join(',', @(1,2,3,4)));
     }
 
     It "truncates prefixed v1.2.3.4.5 to 1,2,3,4" {
-        [String]::Join(',', (Convert-ToVersionArray -InputObject 'V1.2.3.4')) | Should Be ([String]::Join(',', @(1,2,3,4)));
+        [String]::Join(',', (ConvertToAssemblyVersionArray -InputObject 'V1.2.3.4')) | Should Be ([String]::Join(',', @(1,2,3,4)));
     }
 
     It "throws on invalid version string '1'." {
-        { Convert-ToVersionArray -InputObject '1' -ErrorAction Stop } | Should Throw;
+        { ConvertToAssemblyVersionArray -InputObject '1' -ErrorAction Stop } | Should Throw;
     }
 
     It "throws on invalid version string '1.2.a'." {
-        { Convert-ToVersionArray -InputObject '1.2.a' -ErrorAction Stop } | Should Throw;
+        { ConvertToAssemblyVersionArray -InputObject '1.2.a' -ErrorAction Stop } | Should Throw;
     }
 
     It "throws on String.Empty input" {
-        { Convert-ToVersionArray -InputObject ([String]::Empty) } | Should Throw;
+        { ConvertToAssemblyVersionArray -InputObject ([String]::Empty) } | Should Throw;
     }
-
 }
 
 Describe "Get-GitVersionString" {
