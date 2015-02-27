c0fd9f22e831ba53b885fbf4f0dbb06a698abaf6
Iain Brighton <iainbrighton@hotmail.com>
2015-02-27 09:56:38 +0000

    Added importation of the VirtualEngine.Github.ps1 and VirtualEngine.VisualStudio.ps1 to module load

--- VirtualEngine.Build.psm1
+++ VirtualEngine.Build.psm1
@@ -1,9 +1,11 @@
-## Import the VirtualEngine.Module.ps1 and VirtualEngine.Nuget.ps1 files. This permits
-## loading of the module's functions for unit testing, without having to unload/load the module.
+## Import the VirtualEngine .ps1 files. This permits loading of the module's functions
+## for unit testing, without having to unload/load the module.
 . (Join-Path -Path (Split-Path -Path $PSCommandPath) -ChildPath Licenses.ps1);
 . (Join-Path -Path (Split-Path -Path $PSCommandPath) -ChildPath VirtualEngine.Module.ps1);
 . (Join-Path -Path (Split-Path -Path $PSCommandPath) -ChildPath VirtualEngine.NuGet.ps1);
 . (Join-Path -Path (Split-Path -Path $PSCommandPath) -ChildPath VirtualEngine.Git.ps1);
+. (Join-Path -Path (Split-Path -Path $PSCommandPath) -ChildPath VirtualEngine.Github.ps1);
+. (Join-Path -Path (Split-Path -Path $PSCommandPath) -ChildPath VirtualEngine.VisualStudio.ps1);
 
 ## Download Nuget.exe (if not present)
 $moduleRoot = Split-Path -Parent $MyInvocation.MyCommand.Path;
